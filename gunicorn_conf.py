3b90df7d8e4d07ac19409afa1964918d4f9cb2ab
梁文韬 <liangwentao@b.360.cn>
2017-04-15 23:01:33 +0800

    update config

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -4,3 +4,6 @@ bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'meinheld.gmeinheld.MeinheldWorker'
 user = 'root'
+loglevel = 'debug'
+# accesslog = '/var/log/gunicorn_access.log'
+# errorlog = '/var/log/gunicorn_error.log'
